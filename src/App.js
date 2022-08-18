880eb8402fd717290206baef452ee64bda30dbd5
Roey Chang <ronchang8215@gmail.com>
2022-08-18 17:03:02 +0800

    React 讀書會 - Day 14(8/18) - React Router
    練習使用useNavigate()
    題目：https://hackmd.io/2PcaYppLQlCmx7eldhTJ1w

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+// 加入 useNavigate
 import {
   HashRouter,
   NavLink,
   Route,
   Routes,
+  useNavigate
 } from 'react-router-dom';
 
+const Logout = () => {
+    let navigate = useNavigate();
+    return <button onClick={()=> navigate('/login' )}>登出</button>
+}
+
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return <>
+    <p>這是 Todo 頁面</p>
+    <Logout />
+  </>;
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
